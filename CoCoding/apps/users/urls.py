eada0d6ec93269527ce065eb109acf2b993e5418
myungsekyo <myungsekyo@gmail.com>
2020-04-17 23:40:34 +0900

    [users] User API urls added

---
+++ CoCoding/apps/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.users.views import LoginView, UserViewSet
+
+router = DefaultRouter()
+router.register(r'', UserViewSet)
+
+urlpatterns = [
+    path('login/', LoginView.as_view()),
+    path('', include(router.urls)),
+]
