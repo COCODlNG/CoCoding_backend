d1db0cdd374f8e247b91325b8cc0a83d88d860ad
JungChaeMoon <jungchaemoon@gmail.com>
2020-04-18 19:58:15 +0900

    [meetings] Add admin

--- CoCoding/apps/meetings/admin.py
+++ CoCoding/apps/meetings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.meetings.models import Meeting
+from apps.meetings.models import Meeting, MeetingMemberRelation
 
 
 admin.site.register(Meeting)
+admin.site.register(MeetingMemberRelation)
