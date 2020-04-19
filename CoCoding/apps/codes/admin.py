483adefc2c7f60a14919798480a50a0ba95fba80
Jun Seok Lee <shawn950303@gmail.com>
2020-04-19 17:42:19 +0900

    [codes] Modified

--- CoCoding/apps/codes/admin.py
+++ CoCoding/apps/codes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from apps.codes.models import Code
 
-# Register your models here.
+
+admin.site.register(Code)
--- CoCoding/apps/codes/apps.py
+++ CoCoding/apps/codes/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class CodesConfig(AppConfig):
-    name = 'codes'
+    name = 'apps.codes'
