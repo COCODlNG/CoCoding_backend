a98dbd9647ee8cfd7e9b474a82aa85c390790c12
myungsekyo <myungsekyo@gmail.com>
2020-05-10 03:12:53 +0900

    [meetings] add meeting detail, form, list templates

---
+++ CoCoding/apps/meetings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps.meetings.views import MeetingCreateView, MeetingUpdateView, MeetingDetailView, MeetingListView, \
+    MeetingStartView
+
+urlpatterns = [
+    path('', MeetingListView.as_view(), name='list'),
+    path('create/', MeetingCreateView.as_view(), name='create'),
+    path('<int:pk>/', MeetingDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', MeetingUpdateView.as_view(), name='update'),
+    path('<int:pk>/start/', MeetingStartView.as_view(), name='start'),
+]
