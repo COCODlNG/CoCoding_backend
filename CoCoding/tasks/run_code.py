18141477c50b2a892c3f1f91ea7873b1ede581a8
Junseok Lee <shawn950303@gmail.com>
2020-05-11 02:49:49 +0900

    [tasks] add run code task

--- CoCoding/tasks/run_code.py
+++ CoCoding/tasks/run_code.py
@@ -1,4 +1,5 @@
 import subprocess
+import conf.settings
 from celery import Celery
 from apps.codes.models import Code
 
@@ -66,3 +67,53 @@ class CodeExecutor:
                 output_file.get(self.code.language)
             ]
         )
+
+
+def execute_code(code, language_type, pk):
+    C, JAVA, PYTHON = 0, 1, 2
+    code_dir = conf.settings.BASE_DIR + '/tasks/task_code/'
+    if language_type == PYTHON:
+        file_name = pk + '.py'
+        full_path = code_dir + file_name
+        f = open(full_path, 'w')
+        f.write(code)
+        f.close()
+        proc = subprocess.Popen(['python', full_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        return out.decode('utf-8'), err.decode('utf-8')
+    elif language_type == JAVA:
+        file_name = pk + '.java'
+        full_path = code_dir + file_name
+        f = open(full_path, 'w')
+        f.write(code)
+        f.close()
+        proc = subprocess.Popen(['javac', '-d', code_dir, '-sourcepath', code_dir ,full_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #TODO stop code and return compile error message right after compile is finished
+        out, err = proc.communicate()
+        #out.decode('utf-8')
+        #err.decode('utf-8')
+        proc = subprocess.Popen(['java', '-classpath', code_dir, pk], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        return out.decode('utf-8'), err.decode('utf-8')
+    elif language_type == C:
+        file_name = pk + '.c'
+        full_path = code_dir + file_name
+        execute_file_name = code_dir + pk
+        print(execute_file_name)
+        f = open(full_path, 'w')
+        f.write(code)
+        f.close()
+        proc = subprocess.Popen(['gcc', full_path, '-o', execute_file_name], stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        # TODO stop code and return compile error message right after compile is finished
+        out, err = proc.communicate()
+        #out.decode('utf-8')
+        #err.decode('utf-8')
+        proc = subprocess.Popen([execute_file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        return out.decode('utf-8'), err.decode('utf-8')
+    else:
+        return None, None
+
+if __name__ == '__main__':
+    pass
