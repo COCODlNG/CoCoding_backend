82b5bdd47d520d2956e6d47fa7c0757bcd115afb
myungsegyo <myungsekyo@gmail.com>
2020-05-16 15:34:51 +0900

    [static] add meeting_detail.js

---
+++ CoCoding/static/js/meeting_detail.js
@@ -0,0 +1,47 @@
+// =====================================================================================================================
+const C = 'c';
+const JAVA = 'java';
+const PYTHON = 'python';
+
+const C_MODE = 'text/x-csrc';
+const JAVA_MODE = 'text/x-java';
+const PYTHON_MODE = 'python';
+
+const C_TEMPLATE = `
+#include<stdio.h>
+
+void main(){
+    printf("hello C!");
+}
+`;
+
+const PYTHON_TEMPLATE = `
+print('hello python!')
+`;
+
+const JAVA_TEMPLATE = `
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello java!");
+    }
+}
+`;
+
+const LANG_MAP = {
+    [C]: {
+        template: C_TEMPLATE,
+        mode: C_MODE,
+        name: 'C'
+    },
+    [JAVA]: {
+        template: JAVA_TEMPLATE,
+        mode: JAVA_MODE,
+        name: 'Java'
+    },
+    [PYTHON]: {
+        template: PYTHON_TEMPLATE,
+        mode: PYTHON_MODE,
+        name: 'Python'
+    }
+}
+// =====================================================================================================================
