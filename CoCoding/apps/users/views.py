6eb12b08b420c6b909c2bab972568fd73ba08dc2
JungChaeMoon <jungchaemoon@gmail.com>
2020-06-15 07:49:43 +0900

    [signup] add complete message

--- CoCoding/apps/users/views.py
+++ CoCoding/apps/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView as BaseLoginView
+from django.core.exceptions import ImproperlyConfigured
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.generic import RedirectView, CreateView
+from django.contrib import messages
 
 from .forms import UserCreationForm
 
@@ -35,5 +37,8 @@ class LogoutView(RedirectView):
 class UserRegisterView(CreateView):
     template_name = 'register.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('user:login')
+    # success_url = reverse_lazy('index')
 
+    def get_success_url(self):
+        messages.add_message(self.request, messages.SUCCESS, '회원가입이 완료되었습니다.')
+        return reverse_lazy('index')
